739bb763a532632c849f06b5bd7fef2fd1993ba8
Erik Schuessler <90277878+Brainspeck@users.noreply.github.com>
2026-03-31 07:58:46 +0000

    add the logic of the excuse generation and basic styles

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-import "bootstrap";
+
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["My boss", "Trump", "CR7", "Ohtani"];
+let action = ["broke", "hid", "forgot", "threw"];
+let what = ["my toothbrush", "the keys", "the glasses", "my credit card"];
+let when = ["when I was in the bathroom", "in the dog's house", "during the concert", "at the conference"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+let generateExcuse = (who, action, what, when) => {
+  let randomExcuse = who[Math.floor(Math.random() * who.length)] + " " + action[Math.floor(Math.random() * action.length)] + " " + what[Math.floor(Math.random() * what.length)] + " " + when[Math.floor(Math.random() * when.length)];
+  return randomExcuse;
+
+}
+
+ window.onload = function() {
+   //write your code here
+  
+   let displayedExcuse = document.getElementById("excuse");
+   let generateButton = document.getElementById("btn");
+
+   generateButton.onclick = () => {
+     let generatedExcuse = generateExcuse(who, action, what, when);
+      displayedExcuse.innerHTML = generatedExcuse;
+   };
+ };
