a75efe28127aecb8cbbc20f78c9fa759f029ff22
Erik Schuessler <90277878+Brainspeck@users.noreply.github.com>
2026-04-14 04:37:47 +0000

    implement a function to make the code less redundant

--- src/app.js
+++ src/app.js
@@ -8,20 +8,16 @@ let what = ["my toothbrush", "the keys", "the glasses", "my credit card"];
 let when = ["when I was in the bathroom", "in the dog's house", "during the concert", "at the conference"];
 
 
-let generateExcuse = (who, action, what, when) => {
-  let randomExcuse = who[Math.floor(Math.random() * who.length)] + " " + action[Math.floor(Math.random() * action.length)] + " " + what[Math.floor(Math.random() * what.length)] + " " + when[Math.floor(Math.random() * when.length)];
-  return randomExcuse;
-
-}
-
  window.onload = function() {
    //write your code here
-  
+   let generateExcuse = (arr) => {
+    return arr[Math.floor(Math.random() * arr.length)];
+   }
    let displayedExcuse = document.getElementById("excuse");
    let generateButton = document.getElementById("btn");
-
+   
    generateButton.onclick = () => {
-     let generatedExcuse = generateExcuse(who, action, what, when);
+     let generatedExcuse = `${generateExcuse(who)} ${generateExcuse(action)} ${generateExcuse(what)} ${generateExcuse(when)}`;
       displayedExcuse.innerHTML = generatedExcuse;
    };
  };
